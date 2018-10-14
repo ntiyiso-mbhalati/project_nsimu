a7784d7b094ac14a35b728a3fd2afdaf7438cd93
NTIYISO MBHALATI <ntiyisombhalati@NTIYISOs-MacBook-Pro.local>
2018-10-14 08:24:26 +0200

    initial commit

---
+++ android/app/src/main/java/com/example/tinsimu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tinsimu;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
